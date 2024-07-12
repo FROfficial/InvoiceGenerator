c70a78856424b67b6f8c1af8df680c9c153f75dc
FROfficial <fabianramirez051902@gmail.com>
2024-07-11 20:36:54 -0400

    began ui

---
+++ helperFunctions.py
@@ -0,0 +1,3 @@
+# If there is existing invoices the number must reflect that information on the current invoice.
+def createInvoiceTitle(num):
+    return "Invoice_"+str(num)+".pdf"
--- pdfGeneration.py
+++ pdfGeneration.py
@@ -1,4 +1,40 @@
 # Using reportLab to create pdfs which are going to manage all of the invoice creation.
 from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.units import inch
+import helperFunctions
+from tkinter import *
+from tkinter.ttk import *
 
-canvas = Canvas("invoice.pdf")
+# functions used in the UI:
+label_font_reg = ("Myriad", 12)
+label_font_bold = ("Myriad", 12, "bold")
+label_font_italic = ("Myriad", 12, "italic")
+
+def displayInfo():
+    label_text = "Select the Information to generate an invoice."
+    Label(root, text = label_text, font = label_font_reg).pack()
+
+def invoiceNumLabel():
+    label_text = "Invoice number ?"
+    Label(root, text = label_text, font = label_font_reg).pack(side=LEFT)
+    Button(root, text = "Submit").pack(side=RIGHT)
+    Entry(root, width=15).pack(side=RIGHT)
+# MAIN:
+
+# Set up the UI window
+root = Tk()
+root.title('Invoice Generator')
+root.geometry(f"{500}x{500}")
+
+# Design the window
+displayInfo()
+invoiceNumLabel()
+
+# Run the window
+root.mainloop()
+
+my_title = helperFunctions.createInvoiceTitle(256)
+#canvas = Canvas(my_title, pagesize= (8.5 * inch, 11 * inch))
+#canvas.setFont("Times-Roman", 12)
+#canvas.drawString(1 * inch, 10 * inch, "Times New Roman (18 pt)")
+#canvas.save()
